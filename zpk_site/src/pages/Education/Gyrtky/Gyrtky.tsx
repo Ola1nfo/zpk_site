4a8749a827447c38795c643156429e7025f8570e
Ola1nfo <romanovska2807@gmail.com>
2026-01-23 13:26:44 +0200

    samovraduvana added and pablicinfo

--- zpk_site/src/pages/Education/Gyrtky/Gyrtky.tsx
+++ zpk_site/src/pages/Education/Gyrtky/Gyrtky.tsx
@@ -1,35 +1,22 @@
 import './Gyrtky.scss';
 import Header from "../../../components/Header/Header";
 import Footer from "../../../components/Footer/Footer";
-import { motion } from "framer-motion";
+
+//img
+import gyrtky1 from './img/gyrtky1.png';
+import gyrtky2 from './img/gyrtky2.png';
+import gyrtky3 from './img/gyrtky3.png';
 
 export default function Gyrtky() {
   return (
     <div className="gyrtky-page">
       <Header />
 
-      <main className="under-construction">
-        <motion.div
-          className="content"
-          initial={{ opacity: 0, y: 30 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8 }}
-        >
-          <motion.div
-            className="icon"
-            animate={{ rotate: 360 }}
-            transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
-          >
-            🎨
-          </motion.div>
-          <h1>Розділ "Гуртки" в розробці</h1>
-          <p>Ми готуємо для тебе цікаву інформацію про гуртки, творчі студії та позаурочні заходи коледжу.</p>
-
-          <div className="loading-bar">
-            <div className="progress"></div>
-          </div>
-        </motion.div>
-      </main>
+      <div className="gyrtky-content">
+        <img src={gyrtky1} alt="Гурток 1" />
+        <img src={gyrtky2} alt="Гурток 2" />
+        <img src={gyrtky3} alt="Гурток 3" />
+      </div>
 
       <Footer />
     </div>
