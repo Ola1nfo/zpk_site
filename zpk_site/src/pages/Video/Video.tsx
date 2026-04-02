3c9dc0e4eb9be3d79ed9f5b7e28066948e21a52a
Volodymyr <volodymyr@Volodymyrs-MacBook-Air.local>
2026-04-02 11:10:12 +0300

    video url

--- zpk_site/src/pages/Video/Video.tsx
+++ zpk_site/src/pages/Video/Video.tsx
@@ -3,12 +3,12 @@ import { useEffect } from 'react';
 export default function VideoGallery() {
     useEffect(() => {
         // автоматично перекидає на відео
-        window.location.href = "https://www.youtube.com/watch?v=gYno78tASso&t=4s";
+        window.location.href = "https://www.youtube.com/@zpk2026";
     }, []);
 
     return (
         <div>
-            <p>Якщо вас не перекинуло автоматично, <a href="https://www.youtube.com/watch?v=gYno78tASso&t=4s" target="_blank" rel="noopener noreferrer">натисніть тут</a>.</p>
+            <p>Якщо вас не перекинуло автоматично, <a href="https://www.youtube.com/@zpk2026" target="_blank" rel="noopener noreferrer">натисніть тут</a>.</p>
         </div>
     );
 }
